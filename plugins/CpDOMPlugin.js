2190715fa8bee49519c87a50ffdfc1d1c515c6c8
ErikOnBike <erik.stel@gmail.com>
2023-05-12 11:16:17 +0200

    Add primitive for CpDomElement >> #isDescendantOf:

--- plugins/CpDOMPlugin.js
+++ plugins/CpDOMPlugin.js
@@ -337,6 +337,14 @@ console.warn("Process put to sleep because it did not finish in time: " + (proce
       var siblingElement = domElement.nextElementSibling;
       return this.answer(argCount, this.instanceForElement(siblingElement));
     },
+    "primitiveDomElementIsDescendantOf:": function(argCount) {
+      if(argCount !== 1) return false;
+      var parentElement = this.interpreterProxy.stackValue(0).domElement;
+      if(!parentElement) return false;
+      var domElement = this.interpreterProxy.stackValue(argCount).domElement;
+      if(!domElement) return false;
+      return this.answer(argCount, parentElement !== domElement && parentElement.contains(domElement));
+    },
     "primitiveDomElementTagName": function(argCount) {
       if(argCount !== 0) return false;
       var domElement = this.interpreterProxy.stackValue(argCount).domElement;
@@ -1100,6 +1108,7 @@ console.warn("Process put to sleep because it did not finish in time: " + (proce
         if(event.__cp_stop_after === null) {
           isStopped = true;
         } else if(event.__cp_stop_after !== event.__cp_current_target) {
+          // Event bubbled past the currentTarget, value no longer needed
           event.__cp_stop_after = null;
           delete event.__cp_current_target;
           isStopped = true;
