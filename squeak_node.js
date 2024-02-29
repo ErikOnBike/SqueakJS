65835c117a911240699c2718d842f97187e4d91f
ErikOnBike <erik.stel@gmail.com>
2024-02-29 19:28:39 +0100

    Add SessionStorage to sqeuak_node

--- squeak_node.js
+++ squeak_node.js
@@ -64,9 +64,37 @@ Object.assign(global, {
     })
 });
 
+// Add a sessionStorage class
+class SessionStorage {
+	storage = {}
+
+	constructor() {
+		var self = this;
+		Object.keys(process.env).forEach(function(key) {
+			self.storage[key] = process.env[key];
+		});
+	}
+	getItem(name) {
+		return this.storage[name];
+	}
+	setItem(name, value) {
+		this.storage[name] = value;
+	}
+	removeItem(name) {
+		delete this.storage[name];
+	}
+	get length() {
+		return Object.keys(this.storage).length;
+	}
+	key(index) {
+		return Object.keys(this.storage)[index];
+	}
+}
+
 // Extend the new global scope with a few browser/DOM classes and methods
 Object.assign(self, {
     localStorage: {},
+    sessionStorage: new SessionStorage(),
     WebSocket: require("./lib_node/WebSocket"),
     sha1: require("./lib/sha1"),
     btoa: function(string) {
