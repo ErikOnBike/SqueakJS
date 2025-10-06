774c994d20ea11d7f3e7490b2cc45b047f7d7586
ErikOnBike <erik.stel@gmail.com>
2025-10-06 20:46:24 +0200

    Update client version

--- nodeapp.js
+++ nodeapp.js
@@ -38,7 +38,7 @@ class SessionStorage {
     });
 
     // Set environment version (monotonic increasing counter, expecting exact match on server)
-    this.storage["CLIENT_VERSION"] = "7";
+    this.storage["CLIENT_VERSION"] = "8";
   }
   getItem(name) {
     return this.storage[name];
