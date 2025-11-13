ba82f2ec4b49e25154a0dd09d02d94d3ce905c62
ErikOnBike <erik.stel@gmail.com>
2025-11-13 10:37:53 +0100

    Revert earlier changes and apply simpler fix for #resultAs:

--- plugins/CpSystemPlugin.js
+++ plugins/CpSystemPlugin.js
@@ -1077,8 +1077,8 @@ function CpSystemPlugin() {
       }
 
       // Proxy the result, if so requested
-      if(result !== undefined && result !== null && !proxyClass.isNil) {
-        result = this.primHandler.makeStObject(result, proxyClass);
+      if(!proxyClass.isNil) {
+        result = this.resultAs(result, proxyClass);
       }
       return this.answer(argCount, result);
     },
@@ -1106,8 +1106,8 @@ function CpSystemPlugin() {
       var propertyName = this.interpreterProxy.stackValue(1).asString();
       var proxyClass = this.interpreterProxy.stackValue(0);
       var result = obj[propertyName];
-      if(result !== undefined && result !== null && !proxyClass.isNil) {
-        result = this.primHandler.makeStObject(result, proxyClass);
+      if(!proxyClass.isNil) {
+        result = this.resultAs(result, proxyClass);
       }
       return this.answer(argCount, result);
     },
@@ -1215,6 +1215,20 @@ function CpSystemPlugin() {
       var proxyInstance = this.vm.instantiateClass(proxyClass, 0);
       proxyInstance.jsObj = objClass;
       return this.answer(argCount, proxyInstance);
+    },
+    resultAs: function(result, proxyClass) {
+      if(result === undefined || result === null) {
+        return result;
+      }
+      if(proxyClass === this.dictionaryClass) {
+        return this.makeStDictionary(result);
+      }
+      if(proxyClass === this.orderedDictionaryClass) {
+        return this.makeStOrderedDictionary(result);
+      }
+      var proxyInstance = this.vm.instantiateClass(proxyClass, 0);
+      proxyInstance.jsObj = result;
+      return proxyInstance;
     },
     getSelectorNamed: function(obj, selectorName) {
       var selectorDescription = undefined;
