40601004d70a248d51d4ed6ec2d5c2dcc5d28cb0
ErikOnBike <erik.stel@gmail.com>
2025-11-13 10:53:12 +0100

    Revert some left overs

--- plugins/CpSystemPlugin.js
+++ plugins/CpSystemPlugin.js
@@ -218,13 +218,7 @@ function CpSystemPlugin() {
         }
 
         // Dictionary like objects (make exception for the global object)
-        if(proxyClass === thisHandle.dictionaryClass) {
-          return thisHandle.makeStDictionary(obj, seen);
-        }
-        if(proxyClass === thisHandle.orderedDictionaryClass) {
-          return thisHandle.makeStOrderedDictionary(obj, seen);
-        }
-        if(!proxyClass && ((obj.constructor === Object && !thisHandle.hasFunctions(obj)) || (obj.constructor === undefined && typeof obj === "object"))) {
+        if((obj.constructor === Object && !thisHandle.hasFunctions(obj)) || (obj.constructor === undefined && typeof obj === "object")) {
           return thisHandle.makeStOrderedDictionary(obj, seen);
         }
 
