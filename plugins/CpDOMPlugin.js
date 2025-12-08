915bac4a2d7ea29c4e076a8f44333eca820f9919
ErikOnBike <erik.stel@gmail.com>
2025-12-08 11:28:25 +0100

    Revert fix for shadow root exclusion

--- plugins/CpDOMPlugin.js
+++ plugins/CpDOMPlugin.js
@@ -687,8 +687,8 @@ function CpDOMPlugin() {
       if(!domElement) return false;
       var touchedElements = [];
       this.allCpElementsDo(domElement, function(cpElement) {
-        // Add all except the receiver and shadow roots
-        if(cpElement !== receiver && cpElement.domElement.activeElement === undefined) {
+        // Add all except the receiver
+        if(cpElement !== receiver) {
           touchedElements.push(cpElement);
         }
       });
